a76d2951c7664d33d5d56c85f5490daadce45406
Ray Kiddy <ray@ganymede.org>
2015-05-04 14:24:26 -0700

    First version of my application.

---
+++ ccdc/settings.py
@@ -0,0 +1,106 @@
+"""
+Django settings for ccdc project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'hf#kzelpa5^@78$t8&_7u%=0!x#v0=ntkexwjrgtc@1e#8@cfx'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'calaccess_raw',
+    'calaccess_campaign_browser',
+    'tastypie',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'ccdc.urls'
+
+WSGI_APPLICATION = 'ccdc.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'NAME': 'calaccess_raw',
+        'PASSWORD': 'alcibiades99',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'ray',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'local_infile': 1,
+        }
+    }
+}
+
+BUILD_DIR = os.path.join(BASE_DIR, '..', '_build')
+BAKERY_VIEWS = (
+    'campaign_finance.views.IndexView',
+)
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
+
